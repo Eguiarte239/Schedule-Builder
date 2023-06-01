011ae8655c8840324a15d88fd8bc42af8d4e1e11
Eguiarte239 <83661415+Eguiarte239@users.noreply.github.com>
2023-06-01 14:12:25 -0600

    Changed id for title input

--- resources/views/livewire/modal_common_info.blade.php
+++ resources/views/livewire/modal_common_info.blade.php
@@ -2,7 +2,7 @@
     <label for="title" class="block mb-2 text-sm font-medium text-gray-900">
         Title
     </label>
-    <input wire:model="title" type="text" id="first_name"
+    <input wire:model="title" type="text" id="title"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         required>
     <x-jet-input-error for="title"></x-jet-input-error>
