147d438260487c0c6198da4c38b5677bb90e1451
Abraham Israel Lopez Eguiarte <abraham.lopez5085@alumnos.udg.mx>
2023-11-15 09:29:03 -0600

    Se agregó el reestablecimiento de contraseña

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,6 +30,9 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 mr-4 dark:hover:text-white dark:text-white" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
                     <a class="underline text-sm text-gray-600 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white" href="{{ route('register') }}">
                         {{ __('Register') }}
                     </a>
